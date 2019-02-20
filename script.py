66cf3823f2f6cdf4c6e443ff6da12017894563a5
caizhongming <cai.zhongming@hotmail.com>
2019-02-21 00:04:10 +0800

    v

--- script.py
+++ script.py
@@ -1,6 +1,98 @@
-import os
-print "Hello, Python!"
+# -*- coding: utf-8 -*-
+import os, json, time, sys, commands, re
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+#print "Hello, Python!"
+
+(status, output) = commands.getstatusoutput('git status')
+#print output
+str = output.decode('utf-8')
+#print str
+str = "modi a.png modi b.png"
+updateDic = {}
+updateDic['UPDATETIME'] = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+result = re.findall(r"new file:.+resource\/(.+?)\.png", output)
+
+ADDList = []
+for s in result:
+	sd = s.decode('utf-8')
+	(catalog, item) = s.split('/', 1)
+	print catalog, item
+	tmpDic = {}
+	tmpDic['catalog'] = catalog.decode('utf-8')
+	tmpDic['item'] = item.decode('utf-8')
+	ADDList.append(tmpDic)
+updateDic['ADD'] = ADDList
+#print updateDic
+
+deleresult = re.findall(r"deleted:.+resource\/(.+?)\.png", output)
+DELEList = []
+for s in deleresult:
+	sd = s.decode('utf-8')
+	(catalog, item) = s.split('/', 1)
+	print catalog, item
+	tmpDic = {}
+	tmpDic['catalog'] = catalog.decode('utf-8')
+	tmpDic['item'] = item.decode('utf-8')
+	DELEList.append(tmpDic)
+updateDic['DELETE'] = DELEList
+#print updateDic
+
+MODIResult = re.findall(r"modified:.+resource\/(.+?)\.png", output)
+MODIList = []
+for s in MODIResult:
+	sd = s.decode('utf-8')
+	(catalog, item) = s.split('/', 1)
+	print catalog, item
+	tmpDic = {}
+	tmpDic['catalog'] = catalog.decode('utf-8')
+	tmpDic['item'] = item.decode('utf-8')
+	MODIList.append(tmpDic)
+updateDic['MODIFIED'] = MODIList
+#print updateDic
+res = json.dumps(updateDic, ensure_ascii=False)
+#print re
+uf = open('update.json', 'w')
+uf.write(res)
+uf.close()
+
+dic = {}
+dic['UPDATETIME'] = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 for dirpath, dirnames, filenames in os.walk('/Users/caizhongming/Desktop/Ink/resource'):
-    print 'Directory', dirpath
-    for filename in filenames:
-        print ' File', filename
+    
+    if dirpath != '/Users/caizhongming/Desktop/Ink/resource':
+    	list = [];
+    	#print 'Directory', dirpath
+    	fn = dirpath.replace('/Users/caizhongming/Desktop/Ink/resource/','').decode('utf-8')
+    	#print fn
+
+    	#f = open('resource.json', 'w')
+    	#f.write(fn)
+    	#f.close()
+        
+    	for filename in filenames:
+        	#print ' File', filename
+
+        	list.append(filename)
+        dic[fn] = list
+#print dic
+json = json.dumps(dic, ensure_ascii=False)
+print json
+f = open('resource.json', 'w')
+f.write(json)
+f.close()
+
+
+#print result
+#json = json.dumps(result, ensure_ascii=False)
+#print json
+
+
+
+
+
+
+
+
+
+
